037d2d256ed0ac490c3a671fc03c34db00726601
francosparn <francosparn@gmail.com>
2023-06-09 21:00:21 -0300

    commit

--- task/settings.py
+++ task/settings.py
@@ -80,12 +80,7 @@ WSGI_APPLICATION = 'task.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
